10b8d9f9ccd2c5d4de079c4176d35a527a796e88
Maciej Laskowski <laskowski.lmaciej@gmail.com>
2021-07-01 22:26:12 +0200

    Zadanie 13

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,6 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
         Pet dog = new Pet("speciesB");
         Phone iphone = new Phone("apple", "65", "iOS", 5.5, 2019);
         dog.name = "Szarik";
@@ -25,15 +24,34 @@ public class Main {
         Human me = new Human("Maciej", "Laskowski", 18, 5415.3);
         me.mobilePhone = iphone;
         me.pet = dog;
-        me.setCash(200.0);
+        me.setCash(200000.0);
 
-        pig.feed(20.0);
-        pig.beEaten();
+        Application nerflix = new Application("Nerflix", 21.37, 29.99);
+        Application bankApp = new Application("Bank BGC", 31.02, 0.0);
+        Application faceSwap = new Application("FaceSwap", 1.3, 8.99);
+        Application chatter = new Application("Chatter", 9.01, 0.0);
 
+        me.mobilePhone.appInstall(me, nerflix);
+        me.mobilePhone.appInstall(me, bankApp);
+        //me.mobilePhone.appInstall(me, faceSwap);
+        me.mobilePhone.appInstall(me, chatter);
 
-        Human buyer = new Human("Zdzisław", "Nowakowski", 30, 2800.0);
-        buyer.setCash(200000.0);
+        me.mobilePhone.isApplicationInstalled(nerflix);
+        me.mobilePhone.isApplicationInstalled(bankApp);
+        me.mobilePhone.isApplicationInstalled(faceSwap);
+        me.mobilePhone.isApplicationInstalled(chatter);
 
+        me.mobilePhone.isApplicationInstalledByName("Nerflix");
+        me.mobilePhone.isApplicationInstalledByName("Bank BGC");
+        me.mobilePhone.isApplicationInstalledByName("FaceSwap");
+        me.mobilePhone.isApplicationInstalledByName("Chatter");
+
+        me.mobilePhone.allFreeAplications();
+
+        me.mobilePhone.nameSort();
+        me.mobilePhone.priceSort();
+
+        /*
         Car passat = new Car("B1", "Volkswagen", 1997, 300000.0, 10000.0);
         Car passat2 = new Car("B2", "Volkswagen", 1998, 300000.0, 10000.0);
         Car passat3 = new Car("B3", "Volkswagen", 1999, 300000.0, 10000.0);
@@ -64,7 +82,8 @@ public class Main {
         carToSell.transactionNumbers();
         carToSell.doAsellB(me, buyer);
 
-        /*
+
+
         System.out.println("licznik: " + me.getCar().millage);
 
         Car myPassat = me.getCar();
--- src/com/company/device/Phone.java
+++ src/com/company/device/Phone.java
@@ -1,16 +1,20 @@
 package com.company.device;
 
+import com.company.Application;
 import com.company.Human;
+import org.w3c.dom.ls.LSOutput;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Phone extends Device {
     final String operationSystem;
     private Double screenSize;
-    static final String serverAdress =  "adresServera.pl";
+    static final String serverAdress = "adresServera.pl";
     static final String protocol = "https";
     static final Double version = 1.0;
+    public List<Application> apps = new ArrayList<>();
 
     public Phone(String producer, String model, String operationSystem, Double screenSize, Integer productionYear) {
         super(producer, model, productionYear);
@@ -58,19 +62,78 @@ public class Phone extends Device {
             buyer.setCash(buyer.getCash() + price);
         }
     }
-    public void installAnApp(String appName){
+
+    public void installAnApp(String appName) {
 
     }
-    public void installAnApp(String appName, Double version){
+
+    public void installAnApp(String appName, Double version) {
 
     }
-    public void installAnApp(String appName, Double version, String serverAdress){
+
+    public void installAnApp(String appName, Double version, String serverAdress) {
 
     }
-    public void installAnApp(List<String> appsName){
+
+    public void installAnApp(List<String> appsName) {
 
     }
-    public void installAnApp(URL url){
+
+    public void installAnApp(URL url) {
 
     }
+
+    public void appInstall(Human owner, Application application) {
+        if (owner.getCash() >= application.price) {
+            apps.add(application);
+            System.out.println("Aplikacja " + application.name + " została zainstalowana.");
+            owner.setCash(owner.getCash() - application.price);
+            return;
+        }
+        System.out.println("Masz za mało pieniędzy na zakup aplikacji.");
+    }
+
+    public void isApplicationInstalled(Application application) {
+        System.out.println(apps.contains(application) ? "Aplikacja  " + application.name + " jest już zainstalowana." : "Nie znaleziono aplikacji: " + application.name);
+    }
+
+    public void isApplicationInstalledByName(String name) {
+        for (Application app : apps) {
+            if (app.name.equals(name)) {
+                System.out.println("Aplikacja  " +name + " jest zainstalowana");
+                return;
+            }
+        }
+        System.out.println("Nie znaleziono aplikacji: " + name);
+    }
+
+    public void allFreeAplications() {
+        for (Application app : apps) {
+            if (app.price == 0.0) {
+                System.out.println(app.name + " Za darmo!");
+            }
+        }
+    }
+
+    public Double allInstalledAplicationsValue() {
+        Double sum = 0.0;
+        for (Application app : apps) {
+            sum += app.price;
+        }
+        System.out.println("Wartość wszystkich zainstalowanych aplikacji: " + sum);
+        return sum;
+    }
+
+    public void nameSort() {
+        apps.sort((s1, s2) -> s1.name.compareTo(s2.name));
+        apps.forEach(p -> System.out.println(p.name));
+    }
+
+    public void priceSort() {
+        apps.sort((s1, s2) -> s1.price.compareTo(s2.price));
+        apps.forEach(p -> System.out.println(p.name + "cena: " + p.price));
+    }
+
 }
+
+
